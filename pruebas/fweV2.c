cd5229b4abe941a9ceab112ed27962e9d56dc0fb
NyRakath <Soncor23@gmail.com>
2021-11-29 17:43:00 -0500

    fork + wait + execve

---
+++ pruebas/fweV2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * main - fork & wait example
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    pid_t child_pid;
+    int status, i;
+    char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
+
+    for(i = 1; i <= 5; i++)
+    {
+    child_pid = fork();
+    if (child_pid == -1)
+    {
+        perror("Error:");
+        return (1);
+    }
+    if (child_pid == 0)
+        execve(argv[0], argv, NULL);
+    else
+        wait(&status);
+    }
+    return (0);
+}
