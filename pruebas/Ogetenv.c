b3af37aa6ab4fb1183294e2c1d48cec558e67d1a
NyRakath <Soncor23@gmail.com>
2021-12-01 18:25:28 -0500

    New commit

---
+++ pruebas/Ogetenv.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(void)
+{
+    char *aa= NULL;
+    char *env = "PATH";
+    
+    aa = getenv(env);
+	printf("%s", aa);
+	return (0);
+
+}
--- pruebas/_getenv.c
+++ pruebas/_getenv.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
 char *_getenv(const char *name)
 {
     int i = 0;
     char **envi = __environ;
-    char *buff;
+    char *str_compare = NULL;
     
     while (envi[i])
     {
-        strtok(envi, "=");
-        /*
-        if (envi[i] == name)
-        {
-            printf("env: %s\n", envi[i]);
-            return (envi[i]);
-        }*/
+	printf("env[i] : %s\n", envi[i]);
+	str_compare = strtok(envi[i], "=");
+	printf("strtok : %s\n", str_compare);
+	printf("name getenv : %s\n", name);
+	if (str_compare == name)
+	{
+		printf("env: %s\n", envi[i]);
+		return (envi[i]);
+	}
         i++;
     }
-    return (envi[i]);
+    return (NULL);
 }
 
 int main(void)
