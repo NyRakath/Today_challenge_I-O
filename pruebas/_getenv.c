f21cc0bc86db26f921de6d326e9278ff7c89f4c3
David <davidlandg@gmail.com>
2021-12-01 23:45:22 -0500

    [ADD] - asda

--- pruebas/_getenv.c
+++ pruebas/_getenv.c
@@ -5,21 +5,36 @@
 #include <sys/wait.h>
 #include <string.h>
 
+char *_token_after(char *path)
+{
+    char *tokens = NULL;
+
+    for (; tokens = strtok(path, ":"); path = NULL)
+    {
+        printf("%s\n", tokens);
+        if (tokens == NULL)
+        {
+            break;
+        }
+    }
+    return (NULL);
+}
+
 char *_getenv(const char *name)
 {
     int i = 0, dub = 0;
     char **envi = __environ;
     char *str_compare = NULL;
-   /* char *env = "PATH";*/
+    /* char *env = "PATH";*/
 
     while (envi[i])
     {
         str_compare = strtok(envi[i], "=");
-	dub = strcmp(str_compare , name);  
+        dub = strcmp(str_compare, name);
         if (!dub)
         {
-           	str_compare = strtok(NULL, "");
-		return (str_compare);
+            str_compare = strtok(NULL, "");
+            return (str_compare);
         }
         i++;
     }
@@ -28,8 +43,13 @@ char *_getenv(const char *name)
 
 int main(void)
 {
-    char *env = "LS_COLORS";
+    char *env = "PATH";
+    char *env_geted = NULL;
 
-    printf("%s\n", _getenv(env));
+    env_geted = _getenv(env);
+
+    printf("%s\n", env_geted);
+    _token_after(env_geted);
+
     return (0);
 }
